a74380dccd2f7211d45d8653507796c0f482740e
Dom Charlesworth <dgc336@gmail.com>
2016-03-26 09:31:46 +0000

    First set of rules implemented

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eslint-reader.el
-An Emacs Lisp library for reading the settings of your eslint file
+
+This library of utility functions designed to read the settings of
+your eslint file to tell you how to format your code.  The intention
+is to use these functions to set local things like tab width,
+indentation style correctly for the project your in, or to perform
+snippet expansion with the correct spacing.
---
+++ eslint-reader.el
@@ -0,0 +1,81 @@
+;;; eslint-reader.el --- Read an eslint file for usefulness
+
+;; Copyright (C) 2016  Dominic Charlesworth <dgc336@gmail.com>
+
+;; Author: Dominic Charlesworth <dgc336@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Read the contents of an eslint file and parse some of the rules
+;; into useful variables for formatting etc
+
+;;; Code:
+
+(require 'json)
+(require 'flycheck)
+
+(defun eslint-reader-read ()
+  "Read in the .eslintrc file as json."
+  (let* ((eslint-loc (locate-dominating-file (buffer-file-name) flycheck-eslintrc))
+         (eslint-path (format "%s/%s" eslint-loc flycheck-eslintrc))
+         (json-object-type 'plist)
+         (json-plist (json-read-file eslint-path)))
+    (plist-get json-plist :rules)))
+
+(defun eslint-reader-indent-tabs ()
+  "Whether or not to use tabs."
+  (interactive)
+  (let ((rule (plist-get (eslint-reader-read) :indent)))
+    (cond
+     ((vectorp rule) (equal (elt rule 1) "tab")) ;; Indenting set to tab
+     ((eq 2 rule) nil)                           ;; Rule is set to default
+     (t indent-tabs-mode))))                     ;; Rule is not there
+
+(defun eslint-reader-indent-width ()
+  "The indent width of spaces."
+  (interactive)
+  (let ((rule (plist-get (eslint-reader-read) :indent)))
+    (unless (eslint-reader-indent-tabs)
+      (if (vectorp rule) (elt rule 1) 4))))
+
+(defun eslint-reader-semi ()
+  "Whether to add semi colons.
+Returns t if semi colons should be used, nil otherwise"
+  (interactive)
+  (let ((rule (plist-get (eslint-reader-read) :semi)))
+    (if (vectorp rule) (equal (elt rule 1) "always") t)))
+
+(defun eslint-reader-strict ()
+  "Whether or not you need a 'use strict' statement.
+Returns nil if statement is not needed, otherwise t"
+  (interactive)
+  (let ((rule (plist-get (eslint-reader-read) :strict)))
+    (if (vectorp rule) (not (equal (elt rule 1) "never")) t)))
+
+(defun eslint-reader-block-spacing ()
+  "Whether or not you need a 'use strict' statement.
+Returns nil if statement is not needed, otherwise t"
+  (interactive)
+  (let ((rule (plist-get (eslint-reader-read) :block-spacing)))
+    (if (vectorp rule) (equal (elt rule 1) "always") t)))
+
+;;; eslint-reader.el ends here
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; eval: (flycheck-mode 0)
+;; eval: (nameless-mode 0)
+;; End:
