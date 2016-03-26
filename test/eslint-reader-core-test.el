afd1d0f776bdd03c64546321599a05ebfe5df4ed
Dom Charlesworth <dgc336@gmail.com>
2016-03-26 14:50:57 +0000

    New files for core functionality

---
+++ test/eslint-reader-core-test.el
@@ -0,0 +1,14 @@
+;; Tests for the eslint Rule Semi
+;; http://eslint.org/docs/rules/semi
+
+(require 'ert)
+(require 'noflet)
+
+
+
+;;; eslint-reader-semi-test.el ends here
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; eval: (flycheck-mode 0)
+;; eval: (nameless-mode 0)
+;; End:
