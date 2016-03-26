7079682969506bc9e5607bbaf6a647ca1d126012
Dom Charlesworth <dgc336@gmail.com>
2016-03-26 16:41:28 +0000

    Initial commit for the indent rule

---
+++ test/eslint-reader-indent-test.el
@@ -0,0 +1,17 @@
+;; Tests for the eslint Rule indent
+;; http://eslint.org/docs/rules/indent
+
+(require 'ert)
+(require 'noflet)
+
+(defvar code-base-path (f-parent (f-parent (f-this-file))))
+(require 'eslint-reader-core (f-expand "eslint-reader-core.el" code-base-path))
+(require 'eslint-reader-indent (f-expand "eslint-reader-indent.el" code-base-path))
+
+
+;;; eslint-reader-indent-test.el ends here
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; eval: (flycheck-mode 0)
+;; eval: (nameless-mode 0)
+;; End:
